b4bf8655b43b993e58acc4a2b9755add8ce6eaac
GabrielJuniorDev <110058841+Gabrli@users.noreply.github.com>
2023-09-07 19:04:18 +0200

    Update imageSelect.js

--- dist/renders/imageSelect.js
+++ dist/renders/imageSelect.js
@@ -2,19 +2,19 @@ const imgBox = document.querySelector(".logo-box");
 const selectCurrentImage = (data) => {
     let weatherOptions = data.weather[0].main;
     if (weatherOptions === "Clear") {
-        imgBox.style.backgroundImage = "url('../../img/sun.png')";
+        imgBox.style.backgroundImage = "url('https://gabrli.github.io/Weather-app/img/sun.png')";
     }
     else if (weatherOptions === "Rain") {
-        imgBox.style.backgroundImage = "url('../../img/raining.png')";
+        imgBox.style.backgroundImage = "url('https://gabrli.github.io/Weather-app/img/raining.png')";
     }
     else if (weatherOptions === "Clouds") {
-        imgBox.style.backgroundImage = "url('../../img/cloud.png')";
+        imgBox.style.backgroundImage = "url('https://gabrli.github.io/Weather-app/img/cloud.png')";
     }
     else if (weatherOptions === "Mist") {
-        imgBox.style.backgroundImage = "url('../../img/fog.png')";
+        imgBox.style.backgroundImage = "url('https://gabrli.github.io/Weather-app/img/fog.png')";
     }
     else {
-        imgBox.style.backgroundImage = "url('../../img/cloudy.png')";
+        imgBox.style.backgroundImage = "url('https://gabrli.github.io/Weather-app/img/img/cloudy.png')";
     }
 };
 export { selectCurrentImage };
